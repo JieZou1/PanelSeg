1ba80b01f9d03b088df0d238f8838ffab0e48777
Jie Zou <zouj.rpi@gmail.com>
2016-07-08 12:04:42 -0400

    initial commit

--- Java/PanelSeg/src/main/java/gov/nih/nlm/ceb/openi/OpenCVTest.java
+++ Java/PanelSeg/src/main/java/gov/nih/nlm/ceb/openi/OpenCVTest.java
@@ -8,7 +8,14 @@ public class OpenCVTest
 {
     public static void main( String[] args )
     {
-    	String filename = "D:\\Users\\jie\\Openi\\Panel\\data\\Test\\TinySet\\1465-9921-6-6-4.jpg"; 
+    	String filename;
+    	
+    	if (args.length == 0)
+        	filename = "D:\\Users\\jie\\Openi\\Panel\\data\\Test\\TinySet\\1465-9921-6-6-4.jpg";
+    	else if (args.length == 1)
+        	filename = args[0];
+    	else return;
+    	
     	org.bytedeco.javacpp.opencv_core.Mat image = org.bytedeco.javacpp.opencv_imgcodecs.imread(filename);
     	org.bytedeco.javacpp.opencv_highgui.imshow("image", image);
     	org.bytedeco.javacpp.opencv_highgui.waitKey();

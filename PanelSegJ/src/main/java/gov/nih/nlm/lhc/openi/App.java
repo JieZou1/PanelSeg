43fe70ebc98e5d038b7f666286d1f1928d99968f
Jie Zou <zouj.rpi@gmail.com>
2016-08-25 10:40:54 -0400

    Switch to use IntelliJ

---
+++ PanelSegJ/src/main/java/gov/nih/nlm/lhc/openi/App.java
@@ -0,0 +1,13 @@
+package gov.nih.nlm.lhc.openi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

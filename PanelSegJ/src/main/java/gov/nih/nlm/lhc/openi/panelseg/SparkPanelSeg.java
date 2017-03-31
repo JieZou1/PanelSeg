80d9c1d17a82dec456938da23356dc01f3d9ca46
Jie Zou <zouj.rpi@gmail.com>
2017-03-31 11:52:34 -0400

    Spark testing codes

--- PanelSegJ/src/main/java/gov/nih/nlm/lhc/openi/panelseg/SparkPanelSeg.java
+++ PanelSegJ/src/main/java/gov/nih/nlm/lhc/openi/panelseg/SparkPanelSeg.java
@@ -56,8 +56,8 @@ class CopyAsBinaFile implements VoidFunction<String>
         byte[] content = Files.readAllBytes(srcPath);
 
         String imageFile = Paths.get(imagePath).toFile().getName();
-        //Path dstPath = Paths.get("./eval").resolve(imageFile);
-        Path dstPath = Paths.get(imageFile);
+        Path dstPath = Paths.get("./eval").resolve(imageFile);
+        //Path dstPath = Paths.get(imageFile);
         Files.write(dstPath, content);
     }
 }

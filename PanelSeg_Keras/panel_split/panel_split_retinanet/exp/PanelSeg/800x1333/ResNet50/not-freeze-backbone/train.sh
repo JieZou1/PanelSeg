324032ead56adc273567f80ea4c681fb4ef1a766
Jie Zou <zouj.rpi@gmail.com>
2018-08-29 10:44:18 -0400

    experiment with freeze-backbone and not freeze-backbone

--- PanelSeg_Keras/panel_split/panel_split_retinanet/exp/PanelSeg/800x1333/ResNet50/not-freeze-backbone/train.sh
+++ PanelSeg_Keras/panel_split/panel_split_retinanet/exp/PanelSeg/800x1333/ResNet50/not-freeze-backbone/train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 	
-#SBATCH --workdir=/slurm_storage/jzou/programs/PanelSeg/PanelSeg_Keras/panel_split/panel_split_retinanet/exp/PanelSeg/800x1333/ResNet50/freeze-backbone
-#SBATCH --output=/slurm_storage/jzou/programs/PanelSeg/PanelSeg_Keras/panel_split/panel_split_retinanet/exp/PanelSeg/800x1333/ResNet50/freeze-backbone/slurm_%j.out
-#SBATCH --error=/slurm_storage/jzou/programs/PanelSeg/PanelSeg_Keras/panel_split/panel_split_retinanet/exp/PanelSeg/800x1333/ResNet50/freeze-backbone/slurm_%j.error
-#SBATCH --job-name=panel_split_retinanet_train_panelseg_800_1333_resnet50_freeze-backbone
+#SBATCH --workdir=/slurm_storage/jzou/programs/PanelSeg/PanelSeg_Keras/panel_split/panel_split_retinanet/exp/PanelSeg/800x1333/ResNet50/not-freeze-backbone
+#SBATCH --output=/slurm_storage/jzou/programs/PanelSeg/PanelSeg_Keras/panel_split/panel_split_retinanet/exp/PanelSeg/800x1333/ResNet50/not-freeze-backbone/slurm_%j.out
+#SBATCH --error=/slurm_storage/jzou/programs/PanelSeg/PanelSeg_Keras/panel_split/panel_split_retinanet/exp/PanelSeg/800x1333/ResNet50/not-freeze-backbone/slurm_%j.error
+#SBATCH --job-name=panel_split_retinanet_train_panelseg_800_1333_resnet50_not-freeze-backbone
 #SBATCH --gres=gpu:1
 #SBATCH --partition=dgx1
 #SBATCH --mem-bind=local
@@ -19,6 +19,5 @@ python /slurm_storage/jzou/programs/PanelSeg/PanelSeg_Keras/panel_split/panel_sp
     --val-annotations /slurm_storage/jzou/programs/PanelSeg/PanelSeg_Keras/panel_split/panel_split_retinanet/exp/PanelSeg/eval_slurm.csv\
     --epoch 50\
     --backbone resnet50\
-    --freeze-backbone\
     --image-min-side 800\
     --image-max-side 1333
